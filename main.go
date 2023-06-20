150ba2e801b3f2a30f027939adb4a4f6a951ec69
Benjamin Schaff <benjamin.schaff@gmail.com>
2023-06-20 11:49:56 -0400

    Base application

--- main.go
+++ main.go
@@ -31,6 +31,7 @@ func routes() http.Handler {
 func uploadFiles(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
 	t := toolkit.Tools{
@@ -41,6 +42,7 @@ func uploadFiles(w http.ResponseWriter, r *http.Request) {
 	files, err := t.UploadFiles(r, "./uploads")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	out := ""
@@ -52,5 +54,21 @@ func uploadFiles(w http.ResponseWriter, r *http.Request) {
 }
 
 func uploadOneFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
+	t := toolkit.Tools{
+		MaxFileSize:      1024 * 1024 * 1024,
+		AllowedFileTypes: []string{"image/jpeg", "image/png", "image/gif"},
+	}
+
+	f, err := t.UploadOneFile(r, "./uploads")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, _ = w.Write([]byte(fmt.Sprintf("Uploaded %s to the uploads folder, renamed to %s\n", f.OriginalFileName, f.NewFileName)))
 }
